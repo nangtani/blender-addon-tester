e04caddfbeb63341ecbcd93fd12ea99854845634
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-02-28 15:45:32 +0100

    tentative get_blender module not found fix

--- blender_addon_tester/__init__.py
+++ blender_addon_tester/__init__.py
@@ -1,6 +1,6 @@
-from get_blender import get_blender_from_suffix
+from . import get_blender
 
 def test_blender_addon(blender_version="2.80", addon_name="fake_addon", pytests_path="tests/"):
     print("testing ", addon_name, "under", blender_version, "with tests at:", pytests_path)
-    get_blender_from_suffix(blender_version)
+    get_blender.get_blender_from_suffix(blender_version)
     # TODO more steps: run pytests, ensure coverage is OK
