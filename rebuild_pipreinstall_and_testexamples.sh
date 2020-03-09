0642d09544670622e7b2271a6922af303a256f41
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-09 18:16:36 +0100

    adding support for already zipped addons

---
+++ rebuild_pipreinstall_and_testexamples.sh
@@ -0,0 +1,11 @@
+rm -rf build/ dist/ *.egg-info
+pip3 install -r requirements.txt
+pip uninstall blender_addon_tester -y
+python3 setup.py bdist_wheel
+pip install dist/*.whl
+cd examples/testing-*/
+pwd
+for testable_file in $(ls test_fake_addon*.py); do
+  python3 "$testable_file";
+done
+cd ../..
