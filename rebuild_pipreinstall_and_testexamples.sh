d40323bb5416a7f61fc25baede31bc7bce9fc5dc
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-12 18:28:49 +0100

    Github Action:: tentative adding coverage as artifact(7)

--- rebuild_pipreinstall_and_testexamples.sh
+++ rebuild_pipreinstall_and_testexamples.sh
@@ -1,4 +1,5 @@
-export BLENDER_CACHE=~/.blender_cache
+# Set a BLENDER_CACHE is not set yet
+export BLENDER_CACHE=${BLENDER_CACHE:-"~/.blender_cache"}
 rm -rf build/ dist/ *.egg-info
 pip3 install -r requirements.txt
 pip uninstall blender_addon_tester -y
