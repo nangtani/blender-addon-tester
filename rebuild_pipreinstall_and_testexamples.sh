ac8636b9d318d266b735156a1c5fdb88c5f4b07c
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-19 17:02:32 +0100

    #4 tentative fix of windows CI runs

--- rebuild_pipreinstall_and_testexamples.sh
+++ rebuild_pipreinstall_and_testexamples.sh
@@ -1,13 +1,13 @@
 # Set a BLENDER_CACHE is not set yet
 export BLENDER_CACHE=${BLENDER_CACHE:-"~/.blender_cache"}
 rm -rf build/ dist/ *.egg-info
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 pip uninstall blender_addon_tester -y
-python3 setup.py bdist_wheel
+python setup.py bdist_wheel
 pip install dist/*.whl
 cd examples/testing-*/
 pwd
 for testable_file in $(ls test_fake_addon*.py); do
-  python3 "$testable_file";
+  python "$testable_file";
 done
 cd ../..
