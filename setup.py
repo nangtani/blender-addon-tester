caf232a7c45a486ede89df35bd5b4674ea048638
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-25 22:51:19 +0100

    #2 cleaning pip requirements, readding 2.78c

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='blender_addon_tester',
       url='https://github.com/douglaskastle/blender-fake-addon',
       packages=['blender_addon_tester'],
       license = 'MIT',
-      install_requires=['bs4','pytest','requests','pip', 'coverage', 'pytest-cov', 'dmglib'],
+      install_requires=['bs4', 'pytest', 'requests', 'coverage', 'pytest-cov', 'dmglib'],
       long_description=read('README.md'),
       python_requires='>=3',
       include_package_data=True, # Make us of MANIFEST.in including extra files
