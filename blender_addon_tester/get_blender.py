5ffcc5569d7fd1709bdb8e4cfd005f62bc87d0b6
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-03-25 23:05:58 +0100

    #2 troubleshooting 2.79b dmg(3)

--- blender_addon_tester/get_blender.py
+++ blender_addon_tester/get_blender.py
@@ -125,6 +125,7 @@ def getBlender(blender_version, blender_zippath, nightly):
         with attachedDiskImage(blender_zipfile) as mounted_dmg:
             print(f"Mounted {blender_zipfile}")
             print(f"Contents of {mounted_dmg[0]}:", os.listdir(mounted_dmg[0]))
+            print(f"Contents of {mounted_dmg[0]}/Blender:", os.listdir(f"{mounted_dmg[0]}/Blender"))
             print(f'Copying Blender out of mounted space from {mounted_dmg[0]}/Blender.app/Contents to {os.path.realpath(".")}...')
             copy_tree(f'{mounted_dmg[0]}/Blender.app/Contents', os.path.realpath("."))
         zdir = "./"
