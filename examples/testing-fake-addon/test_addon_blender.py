9f1374914d33a1b791f6454b0502c10fbe1223c4
Douglas Kastle <douglas.kastle@gmail.com>
2020-03-26 21:37:22 +0000

    Bulk up script to catch and pass errors

--- examples/testing-fake-addon/test_addon_blender.py
+++ examples/testing-fake-addon/test_addon_blender.py
@@ -1,6 +1,10 @@
 import sys
-import blender_addon_tester as BAT
-
+try:
+    import blender_addon_tester as BAT
+except Exception as e:
+    print(e)
+    sys.exit(1)
+    
 if __name__ == "__main__":
     addon = "fake_addon"
     if len(sys.argv) > 1:
@@ -8,4 +12,9 @@ if __name__ == "__main__":
     else:
         blender_rev = "2.80"
     
-    BAT.test_blender_addon(addon_path=addon, blender_revision=blender_rev)
+    try:
+        exit_val = BAT.test_blender_addon(addon_path=addon, blender_revision=blender_rev)
+    except Exception as e:
+        print(e)
+        exit_val = 1
+    sys.exit(exit_val)
