7e8b13a2eed556fc16a77baed437d317aa5d9977
Douglas Kastle <douglas.kastle@gmail.com>
2020-04-02 22:06:57 +0100

    Removed unnecessary modules from pip

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='blender_addon_tester',
-      version='0.1',
+      version='0.2',
       description='Add-On Tester for Blender using Pytest',
       author='Douglas Kastle',
       author_email='douglas.kastle@gmail.com',
-      url='https://github.com/douglaskastle/blender-fake-addon',
+      url='https://github.com/nangtani/blender-addon-tester',
       packages=['blender_addon_tester'],
       license = 'MIT',
-      install_requires=['bs4', 'pytest', 'requests', 'coverage', 'pytest-cov', 'dmglib'],
+      install_requires=['bs4', 'requests'],
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
-      python_requires='>=3',
+      python_requires='>=3.7',
       include_package_data=True, # Make us of MANIFEST.in including extra files
       classifiers=[
         "Development Status :: 2 - Pre-Alpha",
