1576ac0b7ce69a666eab5c4f5dfc8528bd2b7008
Douglas Kastle <douglas.kastle@gmail.com>
2020-04-02 23:42:42 +0100

    Added built in addon

---
+++ examples/testing-io_scene_obj/test_addon_blender.py
@@ -0,0 +1,25 @@
+import sys
+try:
+    import blender_addon_tester as BAT
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+def main():    
+    if len(sys.argv) > 1:
+        addon = sys.argv[1]
+    else:
+        addon = "fake_addon"
+    if len(sys.argv) > 2:
+        blender_rev = sys.argv[2]
+    else:
+        blender_rev = "2.80"
+    
+    try:
+        exit_val = BAT.test_blender_addon(addon_path=addon, blender_revision=blender_rev)
+    except Exception as e:
+        print(e)
+        exit_val = 1
+    sys.exit(exit_val)
+
+main()
