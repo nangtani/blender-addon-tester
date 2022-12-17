9c95c170f5d07f210224fa4c9cd912a9fb0c7c21
Dave Keeshan <96727608+davekeeshan@users.noreply.github.com>
2022-12-17 15:25:21 +0000

    Remove Programming Lauguagess

--- setup.py
+++ setup.py
@@ -23,12 +23,5 @@ setup(name='blender_addon_tester',
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Artistic Software",
         "License :: OSI Approved :: MIT License"
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3 :: Only',
       ]
      )
