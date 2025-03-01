7152c263aa69fa52245a181ca8e8eb6e6f28a9a3
Dave Keeshan <dave.keeshan@daxzio.com>
2025-03-01 20:29:50 +0000

    FIx local_Addon

--- scripts/load_pytest.py
+++ scripts/load_pytest.py
@@ -25,18 +25,30 @@ class SetupPlugin:
     def pytest_configure(self, config):
         (self.bpy_module, self.zfile) = zip_addon(self.addon, self.addon_dir)
         change_addon_dir(self.bpy_module, self.addon_dir)
-        install_addon(self.bpy_module, self.zfile)
+        install_addon(self.bpy_module, self.zfile, self.addon_dir)
         config.cache.set("bpy_module", self.bpy_module)
 
     def pytest_unconfigure(self):
-#         cmd = "coverage xml"
-#         os.system(cmd)
+        #         cmd = "coverage xml"
+        #         os.system(cmd)
         cleanup(self.addon, self.bpy_module, self.addon_dir)
         print("*** test run reporting finished")
 
 
 try:
-    exit_val = pytest.main(["tests", "-v", "-x", "--cov", "--cov-report", "term-missing", "--cov-report", "xml",], plugins=[SetupPlugin(ADDON)])
+    exit_val = pytest.main(
+        [
+            "tests",
+            "-v",
+            "-x",
+            "--cov",
+            "--cov-report",
+            "term-missing",
+            "--cov-report",
+            "xml",
+        ],
+        plugins=[SetupPlugin(ADDON)],
+    )
 except Exception as e:
     print(e)
     exit_val = 1
