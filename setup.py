1f670c61a16961b93d8f61498f32fe64cc52539a
Dave Keeshan <dave.keeshan@daxzio.com>
2025-03-01 20:46:31 +0000

    Update CI flow

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from blender_addon_tester.version import __version__
 import os
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='blender_addon_tester',
-      version='0.10.2',
+      version=__version__,
       description='Add-On Tester for Blender using Pytest',
       author='Dave Keeshan',
       author_email='dave.keeshan@daxzio.com',
       url='https://github.com/nangtani/blender-addon-tester',
       packages=['blender_addon_tester'],
       license = 'MIT',
-      install_requires=['bs4', 'requests', 'flake8', 'dmglib'],
+      install_requires=['bs4', 'requests', 'flake8', 'dmglib', 'exceptiongroup', 'iniconfig'],
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
       python_requires='>=3.7',
@@ -23,5 +24,12 @@ setup(name='blender_addon_tester',
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Artistic Software",
         "License :: OSI Approved :: MIT License"
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
       ]
      )
